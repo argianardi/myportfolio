80ad1162faaa0696d695c14ba554aaf5e191f20c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-22 14:16:23 +0700

    add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      primary: "Montserrat",
+      secondary: "Orbitron",
+    },
     extend: {
       colors: {
         primary: "#B809C3",
