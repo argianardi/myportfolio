05eb90d688b0281b5f5cdfd7b14b2b2a6d27309b
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-22 23:16:44 +0700

    add portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <>
+      <div id="portfolio" className="section">
+        Portfolio
+      </div>
+    </>
+  );
+};
+
+export default Portfolio;
