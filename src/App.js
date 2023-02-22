2d2eb4a55f5c68e77939c552dcd38ad78f9da716
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-22 21:45:05 +0700

    add Header, Navigation, Banner, Profile, and Portfolio component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Portfolio from "./components/Portfolio";
+import Profile from "./components/Profile";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Navigation />
+      <Banner />
+      <Profile />
+      <Portfolio />
     </div>
   );
 }
