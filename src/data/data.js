abe38a984c4ccb8e41eed26afde49c477ee64544
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-23 09:35:48 +0700

    add projects data

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+import getMoviesImage from "../images/getMoviesImage.png";
+import jitaProfileImage from "../images/jitaProfileImage.png";
+import getPokemonsImage from "../images/getPokemonsImage.png";
+import immersiveDashboardImage from "../images/immersiveDashboardImage.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "getMovies",
+    image: getMoviesImage,
+    sourceCode:
+      "https://github.com/ALTA-FE07-ArgianArdiPrasetya/getMovies/tree/release-4",
+    demo: "https://bukanbioskop.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Jita Profile",
+    image: jitaProfileImage,
+    sourceCode: "https://github.com/argianardi/JitaCom-Profile",
+    demo: "https://jitacom.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Get Pokemons",
+    image: getPokemonsImage,
+    sourceCode: "https://github.com/argianardi/getPokemons",
+    demo: "https://get-pokemons.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Immersive Dashboard",
+    image: immersiveDashboardImage,
+    sourceCode: "https://github.com/argianardi/Immersive-Dashboard",
+    demo: "https://immersive-dashboard-ten.vercel.app/",
+  },
+];
