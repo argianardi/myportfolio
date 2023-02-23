64fd4a412793d69dba6a0a439d7a085ad437ef11
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-23 10:16:51 +0700

    add project style

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { data } from "../data/data";
 
 const Portfolio = () => {
+  const projects = data;
   return (
     <>
-      <div id="portfolio" className="section">
+      <div id="portfolio">
         <div className="container mx-auto max-w-[1200px] px-3 pt-3 pb-20">
           <div className="flex flex-col lg:flex-row justify-between mb-[50px]">
             <div className="order-2 mt-5 lg:order-1">
@@ -20,12 +22,49 @@ const Portfolio = () => {
             </div>
             <div className="order-1 text-center lg:order-2">
               <h2 className="text-secondary font-primary max-w-[700px] mx-auto text-[50px]">
-                07
+                {projects.length}
               </h2>
               <p className="font-semibold font-primary text-fourth">
                 Completed Projects
               </p>
             </div>
+          </div>
+
+          {/* container for projects */}
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+            {/* Gird Item */}
+            {projects.map((item, index) => (
+              <div
+                key={index}
+                // style={{ backgroundImage: `url(${item.image})` }}
+                className="shadow-lg shadow-[#040c16] container rounded-md 
+               text-center  mx-auto h-80"
+              >
+                <img
+                  src={item.image}
+                  alt="item.name"
+                  className="border-2 rounded-md border-b-fourth"
+                />
+                {/* Hover effect for images */}
+                <span className="text-2xl font-bold tracking-wider text-gray-700">
+                  {item.name}
+                </span>
+                <div className="text-center ">
+                  {/* eslint-disable-next-line */}
+                  <a href={item.sourceCode} target="_blank">
+                    <button className="px-4 m-2 text-lg font-bold text-center text-gray-700 rounded-sm hover:bg-slate-200">
+                      Code
+                    </button>
+                  </a>
+                  {/* eslint-disable-next-line */}
+                  <a href={item.demo} target="_blank">
+                    <button className="px-4 m-2 text-lg font-bold text-center text-gray-700 rounded-sm hover:bg-slate-200">
+                      Demo
+                    </button>
+                  </a>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
