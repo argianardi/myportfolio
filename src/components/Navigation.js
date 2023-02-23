d656d2016602776d47b1cb8ea7759bcd5190e239
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-23 23:11:42 +0700

    change skills licon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,15 @@
 import React from "react";
 import Link from "react-scroll/modules/components/Link";
-import {
-  BiHomeAlt,
-  BiUser,
-  BicodeAlt,
-  BiCodeAlt,
-  BiPhoneCall,
-} from "react-icons/bi";
+import { BiUser, BiCodeAlt, BiPhoneCall } from "react-icons/bi";
+import { FaTools } from "react-icons/fa";
 
-import { RiStackFill, RiStackLine } from "react-icons/ri";
 const Navigation = () => {
   return (
     <div className="fixed bottom-4 left-0 right-0 text-center z-10 lg:translate-y-[-50%] lg:top-[50%] lg:left-auto lg:right-5">
       <div className="flex justify-between mx-auto bg-white text-secondary p-2 rounded-full border-secondary border-solid border-[1px] max-w-[200px] lg:flex-col">
         <Link
           to="profile"
-          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer"
+          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full"
           activeClass="active"
           offset={-100}
           smooth={true}
@@ -25,17 +19,17 @@ const Navigation = () => {
         </Link>
         <Link
           to="skills"
-          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer"
+          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full"
           activeClass="active"
           offset={-10}
           smooth={true}
           spy={true}
         >
-          <RiStackLine />
+          <FaTools />
         </Link>
         <Link
           to="portfolio"
-          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer"
+          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full"
           activeClass="active"
           offset={-10}
           smooth={true}
@@ -45,7 +39,7 @@ const Navigation = () => {
         </Link>
         <Link
           to="contact"
-          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer"
+          className="flex w-[50px] h-[50px] justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full"
           activeClass="active"
           offset={-10}
           smooth={true}
