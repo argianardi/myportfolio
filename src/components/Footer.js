0a51bbe82d67c0ea0150abd2c046e23f6ec844a0
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-23 22:58:59 +0700

    fix margin value

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="p-5 mb-20 font-semibold text-center lg:mb-1 font-primary">
+    <div className="p-5 font-semibold text-center font-primary mt-0 sm:mt-[450px] lg:mt-2 mb-96 lg:mb-2">
       {" "}
       &copy; 2023 By Argian Ardi Prasetya
     </div>
