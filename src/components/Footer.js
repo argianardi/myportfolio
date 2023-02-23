c4ba3d8523ed9fdf61277870da2884095d73368a
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-23 18:24:40 +0700

    add margin for bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="p-5 font-semibold text-center font-primary">
+    <div className="p-5 mb-20 font-semibold text-center lg:mb-1 font-primary">
       {" "}
       &copy; 2023 By Argian Ardi Prasetya
     </div>
