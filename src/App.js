135460a23a890a26b00e3845547e9de397df585b
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-23 09:35:15 +0700

    add footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Portfolio from "./components/Portfolio";
@@ -18,6 +19,7 @@ function App() {
       <Navigation />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
