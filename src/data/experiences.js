ceb88d4bc2a6f3356120afb7fc12f1e7b382624b
argianardi <argianardi1435@gmail.com>
2026-05-15 17:53:15 +0700

    feat: add expirience data

---
+++ src/data/experiences.js
@@ -0,0 +1,55 @@
+export const experiences = [
+  {
+    role: 'Frontend Lead',
+    company: 'PT Sellerpintar Digital Asia',
+    companyUrl: 'https://www.linkedin.com/company/sellerpintar/',
+    period: 'May 2025 – April 2026',
+    type: 'Contract',
+    description: [
+       'Leading the frontend development team and coordinating frontend tasks,',
+ 'Providing technical guidance and conducting code reviews to maintain code quality,',
+ 'Managing Git branching workflows and enforcing clean code standards,',
+ 'Working closely with backend teams and designers to ensure timely and high-quality feature delivery.',
+    ],
+    tech: ['Next.js' , 'React.js' , 'Tailwind CSS', 'Cascading Style Sheets (CSS)' , 'JavaScript','TypeScript', 'Git/GitLab'],
+  },
+  {
+    role: 'Frontend Developer',
+    company: 'PT Sellerpintar Digital Asia',
+    companyUrl: 'https://www.linkedin.com/company/sellerpintar/',
+    period: 'Dec 2024 – May 2025',
+    type: 'Contract',
+    description: [
+      'Developed features web applications using HTML, CSS, JavaScript, TypeScript, React JS and Next JS.',
+      'Tested and validated features after development.',
+      'Utilized Git for version control and collaborated with development teams.'
+    ],
+    tech: ['Next.js' , 'React.js' , 'Tailwind CSS', 'Cascading Style Sheets (CSS)' , 'JavaScript','TypeScript', 'Git/GitLab'],
+  },
+  {
+    role: 'Frontend Developer',
+    company: 'PT Bisnis Watch Indonesia',
+    companyUrl: 'https://www.linkedin.com/in/businesswatchindonesia/',
+    period: 'September 2024 – November 2024',
+    type: 'Contract',
+    description: [
+      'Developed features web applications using HTML, CSS, JavaScript, TypeScript and React JS',
+      'Tested and validated features after development.',
+      'Utilized Git for version control and collaborated with development teams.'
+    ],
+    tech: [ 'React.js' , 'Chakra UI', 'Cascading Style Sheets (CSS)' , 'JavaScript','TypeScript', 'Git/GitLab'],
+  },
+  {
+    role: 'Frontend Developer',
+    company: 'PT Daya Rekadigital Indonesia',
+    companyUrl: 'https://www.linkedin.com/company/pt-daya-rekadigital-indonesia/',
+    period: 'Mei 2023 - Maret 2024',
+    type: 'Contract',
+    description: [
+      'Developed features web applications using HTML, CSS, JavaScript, TypeScript and React JS',
+      'Tested and validated features after development.',
+      'Utilized Git for version control and collaborated with development teams.'
+    ],
+    tech: [ 'React.js' , 'Chakra UI', 'Cascading Style Sheets (CSS)' , 'JavaScript','TypeScript', 'Git/GitLab'],
+  },
+];
